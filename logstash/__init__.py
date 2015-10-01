974261dfd27986a8e116c1f48f8a7592cf072e89
zer0def <zer0def@zer0def.0>
2015-10-01 10:12:03 +0200

    * added a basic Redis handler

--- logstash/__init__.py
+++ logstash/__init__.py
@@ -1,13 +1,16 @@
-
 from logstash.formatter import LogstashFormatterVersion0, LogstashFormatterVersion1
 
 from logstash.handler_tcp import TCPLogstashHandler
 from logstash.handler_udp import UDPLogstashHandler, LogstashHandler
+
 try:
     from logstash.handler_amqp import AMQPLogstashHandler
 except:
-   # you need to install AMQP support to enable this handler.
-   pass
+    # you need to install AMQP support to enable this handler.
+    pass
  
-
-
+try:
+    from logstash.handler_redis import RedisLogstashHandler
+except:
+    # need to install Redis support to use this handler
+    pass
---
+++ logstash/handler_redis.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import logging
+import redis
+
+from logstash import formatter
+
+
+class RedisLogstashHandler(logging.Handler):
+    def __init__(self, host='localhost', port=6379, db=0, password=None, 
+                 logging_list='logstash', in_order=True, version=1, tags=None,
+                 message_type='logstash', fqdn=False, *args, **kwargs):
+
+        # init Redis connection and define Redis list used for logging
+        self._redis = redis.Redis(host=host, port=port, db=db, password=password)
+        self._logging_list = logging_list
+
+        # set whether you want the logs to arrive sequentially
+        # by locking a mutex when handling a message to be sent
+        self._in_order = in_order
+
+        # the usual formatter setting business
+        fn = formatter.LogstashFormatterVersion0 if version == 0 \
+             else formatter.LogstashFormatterVersion1
+        self.formatter = fn(message_type, tags, fqdn)
+
+        super(RedisLogstashHandler, self).__init__(*args, **kwargs)
+
+    def emit(self, record):
+        try:
+            self._redis.rpush(self._logging_list, self.format(record))
+        except:
+            self.handleError(record)
+
+    def createLock(self):
+        if self._in_order:
+            super(RedisHandler, self).createLock()
+        else:
+            self.lock = None
